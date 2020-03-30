39e98b2e8cd2a1d0ae3a8b4e66de8f36ecb4f606
TomasEmap <61877861+TomasEmap@users.noreply.github.com>
2020-03-29 22:04:01 -0300

    Update ex-1.31.rkt

--- ex-1.31.rkt
+++ ex-1.31.rkt
@@ -2,10 +2,17 @@
 
 
 (define (product term a next b)
-  "...")
+  (if (> a b)
+      1
+      (* (term a)
+      (product term (next a) next b))))
 
 (define (product-i term a next b)
-  "...")
+  (define (iter a result)
+    (if (> a b)
+    result
+    (iter (next a) (* a result))))
+  (iter a 1)
 
 
 (provide product product-i)
