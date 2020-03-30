948881e27c5f2ced52f3fbf3fb1b73f6c269f905
TomasEmap <61877861+TomasEmap@users.noreply.github.com>
2020-03-29 21:53:17 -0300

    Update ex-1.27.rkt

--- ex-1.27.rkt
+++ ex-1.27.rkt
@@ -11,7 +11,11 @@
 
 
 (define (carmichael-number? n)
-  "...")
+  (define (try-it n a)
+     (cond ((= a 1) #t)
+     ((not (= (expmod a n n) a)) #f)
+     (else (try-it n (- a 1)))))
+  (try-it n (- n 1)))
 
 
 (provide carmichael-number?)
